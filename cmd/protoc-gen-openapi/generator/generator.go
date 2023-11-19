d11aed5af3e6e4772ad573d3a82696e03de3dbd9
Haibo <nomango@qq.com>
2023-11-19 11:30:57 +0800

    fix: only exported

--- cmd/protoc-gen-openapi/generator/generator.go
+++ cmd/protoc-gen-openapi/generator/generator.go
@@ -699,14 +699,14 @@ func (g *OpenAPIv3Generator) addPathsToDocumentV3(d *v3.Document, services []*pr
 				if methodName != "" {
 					defaultHost := proto.GetExtension(service.Desc.Options(), annotations.E_DefaultHost).(string)
 
-					op, path2 := g.buildOperationV3(
-						d, operationID, service.GoName, comment, defaultHost, path, body, inputMessage, outputMessage)
-
 					if *g.conf.OnlyExported {
-						if !strings.HasPrefix(op.Description, "@export") && !strings.HasPrefix(op.Summary, "@export") {
+						if !strings.Contains(comment, "@export") {
 							continue
 						}
 					}
+
+					op, path2 := g.buildOperationV3(
+						d, operationID, service.GoName, comment, defaultHost, path, body, inputMessage, outputMessage)
 
 					// Merge any `Operation` annotations with the current
 					extOperation := proto.GetExtension(method.Desc.Options(), v3.E_Operation)
